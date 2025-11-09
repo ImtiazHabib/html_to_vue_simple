58d233df447654c0c3e5346ed59278bea51dd605
ImtiazHabib <imtiazhabib7@gmail.com>
2025-11-09 18:09:51 +0600

    Markup index code created

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,34 @@
 </script>
 
 <template>
-  
+   <section class="section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+             <h1>Hello welcome to the vue js course Lession</h1>
+          </div>
+        </div>
+      </div>
+   </section>
+
+   <section class="image_card">
+     <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="inner_image_card">
+            <div class="card" style="width: 18rem;">
+              <img src="https://images.pexels.com/photos/19975990/pexels-photo-19975990.jpeg" class="card-img-top" alt="..." width="100px">
+              <div class="card-body">
+                <h5 class="card-title">Image Title</h5>
+                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+                <a href="#" class="btn btn-primary">search it on Pixel</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+     </div>
+   </section>
 </template>
 
 <style scoped>
