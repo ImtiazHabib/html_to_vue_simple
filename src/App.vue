886f796c7e05023eec04e5cb78c6701426fe1489
ImtiazHabib <imtiazhabib7@gmail.com>
2025-11-09 18:15:49 +0600

    dynamic content

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <script setup>
-
+import { image_src, image_title } from './data/data';
+const title = "welcome to My vue Practice code ";
+const width="100px";
 </script>
 
 <template>
@@ -7,7 +9,7 @@
       <div class="container">
         <div class="row">
           <div class="col-md-12">
-             <h1>Hello welcome to the vue js course Lession</h1>
+             <h1>{{ title }}</h1>
           </div>
         </div>
       </div>
@@ -19,9 +21,9 @@
         <div class="col-md-12">
           <div class="inner_image_card">
             <div class="card" style="width: 18rem;">
-              <img src="https://images.pexels.com/photos/19975990/pexels-photo-19975990.jpeg" class="card-img-top" alt="..." width="100px">
+              <img :src="image_src" class="card-img-top" alt="..." :width="width">
               <div class="card-body">
-                <h5 class="card-title">Image Title</h5>
+                <h5 class="card-title">{{ image_title }}</h5>
                 <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
                 <a href="#" class="btn btn-primary">search it on Pixel</a>
               </div>
---
+++ src/data/data.js
@@ -0,0 +1,4 @@
+const image_src = "https://images.pexels.com/photos/19975990/pexels-photo-19975990.jpeg";
+
+const image_title = "Relationship";
+export {image_src,image_title}
